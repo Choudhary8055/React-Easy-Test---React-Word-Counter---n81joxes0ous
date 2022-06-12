09b897cb8756e6b6c6d90dc01ff23a04605d39a8
Choudhary8055 <80520509+Choudhary8055@users.noreply.github.com>
2022-06-12 22:42:24 +0530

    aaa

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,44 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
+  const [value, setValue] = useState("0");
 
-
+  
   return (
+    <>
     <div id="main">
-    </div>
-  )
-}
+      
+      <textarea id='char-limit-input' countLimit={50}/>
+      <div id='word-counter'>
+      <span handelChange={(e)=>{
+        setValue({length: e.target.value.split(" ").length });
+      }}>{value}</span>
+      </div>
 
+      <div id='char-counter' onChange={(e)=>{
+        setValue({value:e.target.value}) >
+    {value}</div>
+
+      <div id='fontSize-input'>
+      <input type="range" min="16" max="32"/>
+    
+      </div>
+      
+    
+  
+      {/* <textarea
+        value={value}
+        onChange={(e) => {
+          setValue({ value: e.target.value });
+          setValue({ length: e.target.value.split(" ").length });
+        }}
+      >
+        Type hear
+      </textarea> */}
+
+    
+    </>
+  );
+};
 
 export default App;
